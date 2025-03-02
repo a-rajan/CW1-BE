ce8b40213b52e0fcd88a54b94727ae390a5895a0
abi <a_rajan@outlook.com>
2025-03-02 19:42:44 +0000

    express

--- b-end.js
+++ b-end.js
@@ -3,3 +3,7 @@ const express = require('express'); // using express.js package/framework
 const { MongoClient, ObjectId } = require('mongodb'); // mongodb package
 const cors = require('cors'); 
 const path = require('path'); // cors + path package
+
+// express app initailization
+const app = express();
+const port = process.env.PORT || 3000; // redner.com + local port
