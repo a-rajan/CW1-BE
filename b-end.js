23cd00158cd25c9f142436dfd2177e9105d8fc93
abi <a_rajan@outlook.com>
2025-03-02 19:44:15 +0000

    dotenv

--- b-end.js
+++ b-end.js
@@ -1,9 +1,9 @@
-// importing relevant packges
+// importing relevant packges/ environments
 const express = require('express'); // using express.js package/framework
 const { MongoClient, ObjectId } = require('mongodb'); // mongodb package
 const cors = require('cors'); 
 const path = require('path'); // cors + path package
+    require('dotenv').config(); // remembered dotenv package
 
 // express app initailization
 const app = express();
-const port = process.env.PORT || 3000; // redner.com + local port
