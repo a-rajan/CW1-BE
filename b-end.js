7777be5056c89f4de5ca54102a731356c2411d11
abi <a_rajan@outlook.com>
2025-03-02 20:04:53 +0000

    middleware

--- b-end.js
+++ b-end.js
@@ -1,9 +1,20 @@
 // importing relevant packges/ environments
 const express = require('express'); // using express.js package/framework
 const { MongoClient, ObjectId } = require('mongodb'); // mongodb package
-const cors = require('cors'); 
+const cors = require('cors');
 const path = require('path'); // cors + path package
-    require('dotenv').config(); // remembered dotenv package
+require('dotenv').config(); // remembered dotenv package
 
 // express app initailization
 const app = express();
+const port = process.env.PORT || 3000; // redner.com + local port
+
+// middleware work below
+app.use(cors()); 
+app.use(express.json()); 
+
+// middleware/logger - sends out requests 
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
